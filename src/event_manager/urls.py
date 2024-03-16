b23831916220bc69214ac112b5bb1a1f329a56d5
Zacharias Dimitrakoudis <zdimitrakoudis@gmail.com>
2024-03-16 19:01:43 +0200

    Included user register and login/refresh/validate token endpoints

--- src/event_manager/urls.py
+++ src/event_manager/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
+
+    # Djoser (register + login/refresh/validate token endpoints)
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.jwt')),
 ]
